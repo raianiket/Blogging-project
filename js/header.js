7c86df932384cb310213c4f5e10edfc9faa2ff15
raianiket <rai078945@gmail.com>
2019-07-12 12:10:32 +0530

    Part A: Creating the Homepage

---
+++ js/header.js
@@ -0,0 +1,13 @@
+var modal = document.getElementById('openSignUp'); 
+window.onclick = function(event) { 
+    if (event.target == modal) { 
+        modal.style.display = "none"; 
+    } 
+} 
+
+var modal = document.getElementById('openSignIn'); 
+window.onclick = function(event) { 
+    if (event.target == modal) { 
+        modal.style.display = "none"; 
+    } 
+} 
