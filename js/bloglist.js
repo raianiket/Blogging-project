63c7d6edd4e0b6d975ec3c6f4065d805c2c58700
raianiket <rai078945@gmail.com>
2019-07-12 20:30:12 +0530

    Part B: Creating list of blogs

---
+++ js/bloglist.js
@@ -0,0 +1,10 @@
+function myTrash(){
+	var popup = document.getElementById("popupp")
+	var myTrash = document.getElementById("image");
+	popup.style.display = "block";
+}
+function noButton(){
+	var popup = document.getElementById("popupp")
+	var myTrash = document.getElementById("image");
+	popup.style.display = "none";
+}
